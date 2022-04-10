7d3d4c640115b1bf9c79fa717f8574c12cc57ef6
Jean Carlos <jeancarllos99@outlook.com>
2022-04-10 19:38:47 -0300

    att

--- Buscador de filmes/scripts/script.js
+++ Buscador de filmes/scripts/script.js
@@ -1,16 +1,18 @@
 
 
 const findFilm = async () => {
+    document.getElementById("filmTitle").innerHTML = `<div class="c-loader"></div>`;
     const film = document.getElementById("boxText").value;
+   
     if (film.length > 0) {
-    const url = `https://imdb-api.com/pt-bt/API/Search/k_9s9o7tmt/${film}`;
+    const url = `https://imdb-api.com/pt-bt/API/Search/k_8kw84xsp/${film}`;
     
         const dados = await fetch(url);
         const filmData = await dados.json();
         const filmId = filmData.results[0].id;
 
         (async () => {
-            const filmInfo = await fetch(`https://imdb-api.com/pt-br/API/Title/k_9s9o7tmt/${filmId}`);
+            const filmInfo = await fetch(`https://imdb-api.com/pt-br/API/Title/k_8kw84xsp/${filmId}`);
             const filmData = await filmInfo.json();
             
             const filmImage = filmData.image;
@@ -43,12 +45,15 @@ function insertFilm(filmImage, filmTitle, filmYear, filmRating, filmDescription,
     document.getElementById("synopsis").innerHTML = filmDescription;
     document.getElementById("filmRating").innerHTML = filmRating;
 
-    document.getElementById("estrelando").innerHTML = "Estrelando: " + `<span>${filmStars}</span>`;
+    if (filmStars.length > 0) {
+        document.getElementById("estrelando").innerHTML = "Estrelando: " + `<span>${filmStars}</span>`;
+    } else {
+        document.getElementById("estrelando").innerHTML = "";
+    }
 
     if (filmWriters.length > 0) {
         document.getElementById("escritores").innerHTML = "Escritores: " + `<span>${filmWriters}</span>`;
     } else {
-        document.getElementById("escritores").innerHTML = "Escritores não informados";
+        document.getElementById("escritores").innerHTML = "";
     }
-
 }
